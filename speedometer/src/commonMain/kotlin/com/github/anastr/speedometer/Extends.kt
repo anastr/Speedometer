f961b3ff04bcb318841bbab7fb417b7830b4eb6f
anastr <anastr244@gmail.com>
2023-07-01 12:37:35 +0400

    Create generateTicks function

---
+++ speedometer/src/commonMain/kotlin/com/github/anastr/speedometer/Extends.kt
@@ -0,0 +1,25 @@
+package com.github.anastr.speedometer
+
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.persistentListOf
+import kotlinx.collections.immutable.toImmutableList
+
+/**
+ * To generate list of stop points for ticks
+ *
+ * @param tickNumber Number of ticks
+ *
+ * @throws IllegalArgumentException If [tickNumber] < 0
+ */
+fun generateTicks(tickNumber: Int): ImmutableList<Float> {
+    require(tickNumber >= 0) { "tickNumber mustn't be negative" }
+
+    return if (tickNumber == 1) {
+        persistentListOf(.5f)
+    } else {
+        buildList {
+            for (i in 0 until tickNumber)
+                add(1f / (tickNumber - 1).toFloat() * i)
+        }.toImmutableList()
+    }
+}
